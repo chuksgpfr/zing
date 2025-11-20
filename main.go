bad230b3f9af991bab1372ba7b8f9bf89547ed8a
chuksgpfr <chuksgpfr@gmail.com>
2025-11-21 00:59:39 +0100

    fixed release

--- main.go
+++ main.go
@@ -11,3 +11,16 @@ func main() {
 	}
 
 }
+
+func Zing() *zing.Services {
+	store, err := zing.NewStore("./zing-store")
+	if err != nil {
+		panic(err)
+	}
+
+	// defer store.Close()
+
+	service := zing.NewServices(store)
+
+	return service
+}
--- zing.go
+++ zing.go
@@ -1,16 +1,14 @@
 package main
 
-import "github.com/chuksgpfr/zing/zing"
+// func Zing() *zing.Services {
+// 	store, err := zing.NewStore("./zing-store")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-func Zing() *zing.Services {
-	store, err := zing.NewStore("./zing-store")
-	if err != nil {
-		panic(err)
-	}
+// 	// defer store.Close()
 
-	// defer store.Close()
+// 	service := zing.NewServices(store)
 
-	service := zing.NewServices(store)
-
-	return service
-}
+// 	return service
+// }
